09c1ed7ffe3bef00132825f011da678327d61ff0
CavanLiu <liuyeying1103@163.com>
2017-03-20 22:40:35 +0800

    新增第一版代码

---
+++ src/main/java/com/energy/WechatPayTrojanApplication.java
@@ -0,0 +1,12 @@
+package com.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPayTrojanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatPayTrojanApplication.class, args);
+	}
+}
