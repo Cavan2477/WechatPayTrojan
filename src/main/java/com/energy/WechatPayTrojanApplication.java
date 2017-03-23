776654e47c1ee41eb8e1917033cb12b6e4d9d96d
CavanLiu <liuyeying1103@163.com>
2017-03-23 08:04:30 +0800

    响应URL配置接口开发

--- src/main/java/com/energy/WechatPayTrojanApplication.java
+++ src/main/java/com/energy/WechatPayTrojanApplication.java
@@ -2,10 +2,20 @@ package com.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WechatPayTrojanApplication
 {
+    @RequestMapping("/test")
+    public String test()
+    {
+        return "This is test page";
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(WechatPayTrojanApplication.class, args);
